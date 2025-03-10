935bba1302d96f056721bd6974c4ff71cadaae47
Tatiana Anderson <happytasha9933@gmail.com>
2025-03-10 14:59:12 +0400

    add tabs for all type of card add js to choose

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 
 const tabsBtn = document.querySelectorAll('.tabs__btn-item');
+const tabContent = document.querySelectorAll('.tabs__content-item');
 
 tabsBtn.forEach(function(element){
     element.addEventListener('click', open);
@@ -7,12 +8,21 @@ tabsBtn.forEach(function(element){
 
 function open(event){
     const tabTarget = event.currentTarget;
+    const button = tabTarget.dataset.button;
 
     tabsBtn.forEach(function(item){
         item.classList.remove('tabs__btn-item--active');
-    })
+    });
 
     tabTarget.classList.add('tabs__btn-item--active');
+
+    tabContent.forEach(function(item){
+        item.classList.remove('tabs__content-item--active');
+    });
+
+    const res = document.querySelector(`#${button}`).classList.add('tabs__content-item--active');
+    console.log(res)
+
 }
 
 
