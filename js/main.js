c4bf20ccf658f123d79f8ad6dc6776fb591ecf15
Tatiana Anderson <happytasha9933@gmail.com>
2025-03-10 11:37:35 +0400

    add function for tabs

--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
+
+const tabsBtn = document.querySelectorAll('.tabs__btn-item');
+
+tabsBtn.forEach(function(element){
+    element.addEventListener('click', open);
+})
+
+function open(event){
+    const tabTarget = event.currentTarget;
+
+    tabsBtn.forEach(function(item){
+        item.classList.remove('tabs__btn-item--active');
+    })
+
+    tabTarget.classList.add('tabs__btn-item--active');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const swiper = new Swiper(".swiper", {
     effect: "fade",
     pagination: {
